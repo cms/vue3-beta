97c01be6fa7fa70f5d23b04870bf8a2805f92923
Christian C. Salvado <cmsalvado@vonap.com>
2020-04-21 12:12:02 -0600

    feat: add Vue 3 beta

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
@@ -21,10 +18,9 @@ const routes = [
   }
 ];
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
+const router = new createRouter({
+  history: createWebHistory(),
+  routes,
+})
 
 export default router;
